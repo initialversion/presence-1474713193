45bd756e539a35b1ae405337b496e090e9a29f76
Murugan <murugu2012@gmail.com>
2016-09-24 16:03:59 +0530

    Generate users resource with starter:resource

---
+++ db/migrate/20160924103359_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :last_name
+      t.integer :integer1
+      t.string :integer2
+      t.integer :integer3
+
+      t.timestamps
+
+    end
+  end
+end
